7ee8cf06c337f97b8666fb6ba049a48393526e19
Your Name <you@example.com>
2021-08-11 15:44:49 -0500

    FromVisTest and Video.js Installed also Typescript

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         UI/UX PORTFOLIO APP Update Comp with Local APP
+         video.js and typescript installed
         </p>
         <a
           className="App-link"
